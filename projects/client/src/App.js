3196e4365bbdfbddc19a1511918b4294ff4dd449
mnaufal98 <mnaufal98@gmail.com>
2023-07-15 00:00:33 +0700

    JCWD240202-13

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -1,19 +1,23 @@
-import { Routes, Route } from "react-router-dom";
-import Homepage from "./pages/user/homepage";
-import Navbar from "./components/user/navbar/navbar";
-
+import { Routes, Route } from 'react-router-dom';
+import Homepage from './pages/user/homepage';
+import Navbar from './components/user/navbar/navbar';
+import RegisterPage from './pages/user/registerPage';
+import LoginPage from './pages/user/loginPage';
+import VerificationPage from './pages/user/verificationPage';
 
 function App() {
-  
-  return (
-    <>
-    <Navbar />
-    
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-    </Routes>
-    </>
-  );
+    return (
+        <>
+            <Navbar />
+
+            <Routes>
+                <Route path="/" element={<Homepage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/verification" element={<VerificationPage />} />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
