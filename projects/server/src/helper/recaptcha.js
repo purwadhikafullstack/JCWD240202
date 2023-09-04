99cc711f236cb6a9e0b951fb0b9856120c6c066c
Ryan Defryan <muhammaddefryan@gmail.com>
2023-09-04 20:51:20 +0700

    Update recaptcha.js

--- projects/server/src/helper/recaptcha.js
+++ projects/server/src/helper/recaptcha.js
@@ -4,7 +4,7 @@ module.exports = {
     googleRecaptcha: async (tokenRecaptcha) => {
         try {
             const Recaptcha = await axios.post(
-                `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_API_KEY}&response=${tokenRecaptcha}`,
+                `https://www.google.com/recaptcha/api/siteverify?secret=6LeDOeYnAAAAAGrV2Jd0n48Oq57Qu-Q2E29A7aub&response=${tokenRecaptcha}`,
             );
 
             return Recaptcha.data.success
