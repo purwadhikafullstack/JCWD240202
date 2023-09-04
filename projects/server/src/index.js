2867c5d3056422a54aadf698109ee8e1a164523b
Ryan Defryan <muhammaddefryan@gmail.com>
2023-09-04 15:58:37 +0700

    Update index.js

--- projects/server/src/index.js
+++ projects/server/src/index.js
@@ -37,7 +37,8 @@ app.use(cors());
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static('src/public/images'));
+// app.use(express.static('src/public/images'));
+app.use(express.static(path.join(__dirname, 'public/images')));
 app.use(express.static('public'));
 // app.use(express.static(path.join(__dirname, 'public')));
 
